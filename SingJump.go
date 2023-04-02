6db9060fa5536634c47de577cc22cf958a2fed5d
LXGMAX <leickwell@hotmail.com>
2023-04-02 17:31:31 +0800

    update key operation add get vol api

---
+++ SingJump.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/go-vgo/robotgo"
+	"github.com/itchyny/volume-go"
+)
+
+const (
+	KeyPause 		= "p"
+	KeyNext  		= "right"
+	KeyPrevious = "left"
+	KeyVolDown 	= "down"
+	KeyVolUp 		= "up"
+	KeyLyric 		= "d"
+	KeyLike 		= "l"
+)
+
+const (
+	AudioMute		 = "audio_mute"
+	AudioVolDown = "audio_vol_down"
+	AudioVolUp   = "audio_vol_up"
+	AudioPlay    = "audio_play"
+	AudioPrev    = "audio_prev"
+	AudioNext    = "audio_next"
+)
+
+func keyOperate(key string) {
+	robotgo.KeyTap(key, "ctrl", "alt")
+}
+
+func audioPlay(play string) {
+	robotgo.KeyTap(play)
+}
+
+func audioGetVol() int {
+	volume, err := volume.GetVolume()
+	if err != nil {
+		fmt.Println("vol err: ", err)
+	}
+	return volume
+}
+
+func audioGetMuteStat() bool {
+	mute, err := volume.GetMuted()
+	if err != nil {
+		fmt.Println("get mute err: ", err)
+	}
+	return mute
+}
+
+func playPause(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintln(w, "{\"play\": \"pause\"}")
+	audioPlay(AudioPlay)
+}
+
+func playNext(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintln(w, "{\"play\": \"next\"}")
+	audioPlay(AudioNext)
+}
+
+func playPrevious(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintln(w, "{\"play\": \"previous\"}")
+	audioPlay(AudioPrev)
+}
+
+func playLike(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	keyOperate(KeyLike)
+	fmt.Fprintln(w, "{\"play\": \"like\"}")
+}
+
+func playVolumeUp(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	audioPlay(AudioVolUp)
+	vol := audioGetVol()
+	fmt.Fprintln(w, "{\"play\": \"volup\", \"volume\": ", vol, "}")
+}
+
+func playVolumeDown(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	audioPlay(AudioVolDown)
+	vol := audioGetVol()
+	fmt.Fprintln(w, "{\"play\": \"voldown\", \"volume\": ", vol, "}")
+}
+
+func playLyric(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	keyOperate(KeyLyric)
+	fmt.Fprintln(w, "{\"play\": \"lyric\"}")
+}
+
+func playMute(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	audioPlay(AudioMute)
+	vol := audioGetVol()
+	if audioGetMuteStat() == true {
+		vol = 0
+	}
+	fmt.Fprintln(w, "{\"play\": \"mute\", \"volume\": ", vol, "}")
+}
+
+func sysGetVol(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	vol := audioGetVol()
+	fmt.Fprintln(w, "{\"volume\": ", vol, "}")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/play/pause", playPause)
+	router.GET("/play/next", playNext)
+	router.GET("/play/previous", playPrevious)
+	router.GET("/play/volup", playVolumeUp)
+	router.GET("/play/voldown", playVolumeDown)
+	router.GET("/play/like", playLike)
+	router.GET("/play/lyric", playLyric)
+	router.GET("/play/mute", playMute)
+	router.GET("/sys/getvol", sysGetVol)
+
+	err := http.ListenAndServe(":18890", router)
+	if err != nil {
+		panic(err)
+	}
+}
